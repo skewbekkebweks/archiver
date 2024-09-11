39dfbbab488f998464eca22ad9aa61cce831a2e0
Mikhail Glazov <mikhail.glazov2015@yandex.ru>
2024-09-12 01:00:08 +0300

    task chocolate done

--- tasks/chocolate/chocolate.cpp
+++ tasks/chocolate/chocolate.cpp
@@ -1,5 +1,13 @@
 #include "chocolate.h"
 
 bool CanBreakSlices(int height, int width, int slices_amount) {
-    return true;
+    if (slices_amount <= 0 || slices_amount >= height * width) {
+        return false;
+    }
+
+    if (slices_amount % height == 0 || slices_amount % width == 0) {
+        return true;
+    }
+
+    return false;
 };
