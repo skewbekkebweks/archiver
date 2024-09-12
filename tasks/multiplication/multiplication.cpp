53599ce76e082eebc600fee557e1828b6a728778
Mikhail Glazov <mikhail.glazov2015@yandex.ru>
2024-09-12 15:29:51 +0300

    finally solved multiplication

--- tasks/multiplication/multiplication.cpp
+++ tasks/multiplication/multiplication.cpp
@@ -1,5 +1,5 @@
 #include "multiplication.h"
 
 int64_t Multiply(int a, int b) {
-    return static_cast<int64_t>(a) * static_cast<int64_t>(b); 
+    return static_cast<int64_t>(a) * static_cast<int64_t>(b);
 }
