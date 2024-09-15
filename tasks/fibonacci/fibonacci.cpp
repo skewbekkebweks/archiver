6a5355514102512cc05c03d868d41b7879efca14
Mikhail Glazov <mikhail.glazov2015@yandex.ru>
2024-09-15 08:53:34 +0300

    fix solution

--- tasks/fibonacci/fibonacci.cpp
+++ tasks/fibonacci/fibonacci.cpp
@@ -3,7 +3,7 @@
 int64_t CalculateNthFibonacci(int64_t n) {
     int64_t fibonacci_n = 0;
     int64_t fibonacci_n_plus_1 = 1;
-    
+
     for (int64_t i = 1; i <= n; ++i) {
         int64_t fibonacci_n_plus_2 = fibonacci_n + fibonacci_n_plus_1;
         fibonacci_n = fibonacci_n_plus_1;
