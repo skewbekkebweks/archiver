e84750e0d076e63fac560b1d7f7a2da05b2cbcf3
Notmany Task <notmanytask@bigredeye.me>
2024-09-15 09:24:27 +0000

    submits/reduce_fraction

--- tasks/reduce_fraction/reduce_fraction.cpp
+++ tasks/reduce_fraction/reduce_fraction.cpp
@@ -1,6 +1,20 @@
 #include "reduce_fraction.h"
 
+int64_t Gcd(int64_t a, int64_t b) {
+    if (b == static_cast<int64_t>(0)) {
+        return a;
+    }
+    if (a < b) {
+        return Gcd(b, a);
+    }
+    return Gcd(b, a % b);
+}
 
 int64_t ReduceFraction(int64_t numerator, int64_t denominator) {
-    return 1;
+    int64_t gcd = Gcd(numerator, denominator);
+
+    numerator /= gcd;
+    denominator /= gcd;
+
+    return numerator + denominator;
 }
