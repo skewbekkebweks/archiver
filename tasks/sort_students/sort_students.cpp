3ab0c15cc613a0522da7023bc4023a75966388d5
Mikhail Glazov <mikhail.glazov2015@yandex.ru>
2024-09-20 21:00:43 +0300

    sort_students task done

--- tasks/sort_students/sort_students.cpp
+++ tasks/sort_students/sort_students.cpp
@@ -1,4 +1,16 @@
 #include "sort_students.h"
 
-void SortStudents(std::vector<Student>& students, SortKind sortKind) {
+bool CmpStudents(Student& s1, Student& s2, SortKind sort_kind) {
+    if (sort_kind == SortKind::Name) {
+        return std::tie(s1.last_name, s1.name, s1.birth_date.year, s1.birth_date.month, s1.birth_date.day) <
+               std::tie(s2.last_name, s2.name, s2.birth_date.year, s2.birth_date.month, s2.birth_date.day);
+    } else {
+        return std::tie(s1.birth_date.year, s1.birth_date.month, s1.birth_date.day, s1.last_name, s1.name) <
+               std::tie(s2.birth_date.year, s2.birth_date.month, s2.birth_date.day, s2.last_name, s2.name);
+    }
+}
+
+void SortStudents(std::vector<Student>& students, SortKind sort_kind) {
+    sort(students.begin(), students.end(),
+         [&sort_kind](Student& s1, Student& s2) -> bool { return CmpStudents(s1, s2, sort_kind); });
 }
