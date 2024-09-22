0c94e14bb2be52855422634b020cf9657e2f6cf4
Mikhail Glazov <mikhail.glazov2015@yandex.ru>
2024-09-22 16:14:44 +0300

    add std::deque in solution

--- tasks/tests_checking/tests_checking.cpp
+++ tasks/tests_checking/tests_checking.cpp
@@ -1,33 +1,26 @@
 #include "tests_checking.h"
-#include <string>
-#include <vector>
+#include <deque>
 
 std::vector<std::string> StudentsOrder(const std::vector<StudentAction>& student_actions,
                                        const std::vector<size_t>& queries) {
-    int count_top_actions = 0;
+    std::deque<std::string> ordred_student_names_deque;
     for (const StudentAction& student_action : student_actions) {
-        if (student_action.side == Side::Top) {
-            count_top_actions++;
+        switch (student_action.side) {
+            case Side::Top:
+                ordred_student_names_deque.push_front(student_action.name);
+                break;
+            case Side::Bottom:
+                ordred_student_names_deque.push_back(student_action.name);
+                break;
         }
     }
 
-    std::vector<std::string> student_names_in_order(student_actions.size());
-    int next_top_index = count_top_actions - 1;
-    int next_bottom_index = count_top_actions;
-
-    for (const StudentAction& student_action : student_actions) {
-        if (student_action.side == Side::Top) {
-            student_names_in_order[next_top_index] = student_action.name;
-            next_top_index--;
-        } else {
-            student_names_in_order[next_bottom_index] = student_action.name;
-            next_bottom_index--;
-        }
-    }
+    std::vector<std::string> ordered_student_names_vector(
+        {ordred_student_names_deque.begin(), ordred_student_names_deque.end()});
 
     std::vector<std::string> response;
     for (const size_t& query : queries) {
-        response.push_back(student_names_in_order[query - 1]);
+        response.push_back(ordered_student_names_vector[query - 1]);
     }
 
     return response;
