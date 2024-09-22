bfdea246dcd5aa362e0327ca9720d87fc7df59b9
Notmany Task <notmanytask@bigredeye.me>
2024-09-22 11:21:48 +0000

    submits/palindrome

--- tasks/palindrome/palindrome.cpp
+++ tasks/palindrome/palindrome.cpp
@@ -1,5 +1,38 @@
 #include "palindrome.h"
 
 bool IsPalindrome(const std::string& str) {
-    return false;
+    if (str.empty()) {
+        return true;
+    }
+
+    size_t left_pointer = 0;
+    size_t right_pointer = str.size() - 1;
+
+    while (true) {
+        while (left_pointer < str.size() && str[left_pointer] == ' ') {
+            left_pointer++;
+        }
+
+        if (left_pointer == str.size()) {
+            // all elements are checked
+            return true;
+        }
+
+        while (str[right_pointer] == ' ') {  // if left_pointer < str.size() then right_pointer will be >= 0
+            right_pointer--;
+        }
+
+        if (str[left_pointer] != str[right_pointer]) {
+            return false;
+        }
+
+        if (left_pointer == str.size() - 1 || right_pointer == 0) {
+            // all elemnets are checked
+            return true;
+        }
+
+        left_pointer++;
+        right_pointer--;
+    }
+    return true;
 }
