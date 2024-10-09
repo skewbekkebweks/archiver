7e63183c2f21ca1f83b5b892d1245a201fba716c
Mikhail Glazov <mikhail.glazov2015@yandex.ru>
2024-10-09 19:12:09 +0300

    stack solved

--- tasks/stack/stack.cpp
+++ tasks/stack/stack.cpp
@@ -1,1 +1,43 @@
 #include "stack.h"
+
+
+Node::Node(int32_t value, Node* previous) : value(value), previous(previous) {}
+
+Stack::Stack() : head_(nullptr), size_(0) {}
+
+Stack::~Stack() {
+    Clear();
+}
+
+void Stack::Push(int32_t value) {
+    Node* n = new Node{value, head_};
+    head_ = n;
+    size_++;
+}
+
+void Stack::Pop() {
+    Node* to_delete = head_;
+    head_ = head_->previous;
+
+    delete to_delete;
+    size_--;
+}
+
+int32_t Stack::Top() const {
+    return head_->value;
+}
+
+int32_t Stack::Size() const {
+    return size_;
+}
+
+void Stack::Clear() {
+    while (!Empty()) {
+        Pop();
+    }
+    size_ = 0;
+}
+
+bool Stack::Empty() const {
+    return size_ == 0;
+}
