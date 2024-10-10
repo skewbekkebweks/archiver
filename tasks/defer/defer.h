3afdf3724fac4beacedb405b3bb51352a3297160
Mikhail Glazov <mikhail.glazov2015@yandex.ru>
2024-10-10 16:10:32 +0300

    probably defer done

--- tasks/defer/defer.h
+++ tasks/defer/defer.h
@@ -3,12 +3,13 @@
 
 class Defer {
 public:
-    Defer(auto func) : func_(func) { // NOLINT
+    Defer(auto func) : func_(func) {  // NOLINT
     }
 
     ~Defer() {
         func_();
     }
+
 private:
     std::function<void()> func_;
 };
@@ -17,4 +18,3 @@ private:
 #define COMBINE(X, Y) COMBINE_HELPER(X, Y)
 #define UNIQUE_NAME(name) COMBINE(name, __LINE__)
 #define defer Defer UNIQUE_NAME(var_) = [&]() -> void
-
