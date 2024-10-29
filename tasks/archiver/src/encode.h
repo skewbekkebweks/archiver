6bc93d0a86608c9e6b2bb6668fe85e9b7c91b450
Mikhail Glazov <mikhail.glazov2015@yandex.ru>
2024-10-29 23:39:34 +0300

    fox codestyle

--- tasks/archiver/src/encode.h
+++ tasks/archiver/src/encode.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "trie.h"
+#include "write_bit_stream.h"
 
 #include <cstdint>
 #include <string>
@@ -16,5 +17,10 @@ std::map<uint16_t, uint16_t> GetCodesSizeCount(const std::map<uint16_t, int>& fr
 std::vector<std::pair<uint16_t, std::string>> GenerateCodes(const std::map<uint16_t, int>& frequency_map);
 
 std::map<uint16_t, int> GenerateFrequencyMap(const std::string& filename, int coef_speacial = 1, bool add_name = true);
+
+void WriteFile(WriteBitStream& writer, const std::string filename,
+               const std::vector<std::pair<uint16_t, std::string>>& ordered_codes,
+               const std::map<uint16_t, std::string>& codes, const std::map<size_t, int16_t>& count_with_size,
+               size_t max_sybmol_code_size);
 
 void Encode(const std::string& archive_name, const std::vector<std::string>& filenames);
